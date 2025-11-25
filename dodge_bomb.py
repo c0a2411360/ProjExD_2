9ee872ffa3f79d7fea177adef24b7bb2f0f0563a
saki <c0a2411360@edu.teu.ac.jp>
2025-11-25 14:42:21 +0900

    練習問題4:衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,7 +48,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+            
+        if kk_rct.colliderect(bb_rct):  #こうかとんと爆弾が衝突したら
+            print("ゲームオーバー")
+            return 
+        
+        screen.blit(bg_img, [0, 0])
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         # if key_lst[pg.K_UP]:
